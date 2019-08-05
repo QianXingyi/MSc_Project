4c1d252d40ab7a9bfdde95b2d059936f84fae914
QianXingyi <jsjyqxy@gmail.com>
2019-08-05 14:56:32 +0100

    ADD TTS

--- app/src/main/java/cn/moecity/myapplication/MainActivity.java
+++ app/src/main/java/cn/moecity/myapplication/MainActivity.java
@@ -120,6 +120,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSpeech = new TextToSpeech(MainActivity.this, new TTSListener());
         latLngView = findViewById(R.id.textView);
         listView = findViewById(R.id.showNode);
         createLocationRequest();
